3072156563465eff144df96fc9995520433c1fc4
DD <ddonegal1@gmail.com>
2024-01-10 10:06:22 -0500

    final commit

--- src/helpers.js
+++ src/helpers.js
@@ -4,6 +4,11 @@ function choice (items) {
 }
 
 function remove (items, item) {
+    if (!Array.isArray(items)) {
+        console.error('Input is not an array or is undefined.');
+        return undefined; // or handle the error in an appropriate way
+      }
+      
    for(let i=0; i < items.length; i++){
     if(items[i] === item){
         return [...items.slice(0, i), ...items.slice(i+1)];
--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ console.log(`I’d like one ${randomFruit} please.`)
 console.log(`Here you go: ${randomFruit}`)
 console.log(`Delicious! May I have another?`);
 
-let remaining = remove(randomFruit, fruits);
+let remaining = remove(fruits, randomFruit);
 console.log(`I’m sorry, we’re all out. We have ${remaining.length} left.`);
